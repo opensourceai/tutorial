da1070b6c4e23e126d2b8d039b225d7d9d315bc0
Chen Quan <chenquan@osai.club>
2019-02-27 21:21:39 +0800

    update tensorflow/basic_model

--- tensorflow/example/basic_model/logistic_regression.py
+++ tensorflow/example/basic_model/logistic_regression.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import train_test_split
 __author__ = 'Chen Quan'
 # 读取数据
 X, Y = load_breast_cancer(True)
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=2019)
 # Parameters
 learning_rate = 0.01
 training_epochs = 25
